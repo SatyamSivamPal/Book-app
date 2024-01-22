bac9cd42f4825bf61b1024110b0646dba1acf712
SatyamSivamPal <palsatyam1968@gmail.com>
2024-01-22 07:28:34 +0530

    CRUD

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ app.use(express.json());
 
 //databse connection
 connectToMongoDB()
-const uri = "mongodb://127.0.0.1:27017/books?directConnection=true";
+const uri = "mongodb://127.0.0.1:27017/?directConnection=true";
 
 const client = new MongoClient(uri);
 
 const bookCollection = client.db("BookInvetory").collection("books")
 
 //insert books
-app.post('/upload', async(req,res) => {
+app.post('/upload', async (req, res) => {
     const data = req.body
     const result = await bookCollection.insertOne(data)
     res.send(result);
@@ -34,15 +34,40 @@ app.get("/all-books", async(req, res) => {
 })
 
 //update books data
-app.patch("/books/:id", async(req, res) => {
+app.patch("/books/:id", async (req, res) => {
     const id = req.params.id;
     const updateBookData = req.body;
-    const filter = {_id: new ObjectId(id)};
-    const options = {upsert: true};
+    const filter = { _id: new ObjectId(id) };
 
     const updateDoc = {
-        
+        $set: {
+            ...updateBookData
+        }
     }
+
+    const options = { upsert: true };
+    const result = await bookCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+})
+
+//delete book
+app.delete("/books/:id", async(req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)};
+    const result = await bookCollection.deleteOne(filter);
+    res.send(result)
+})
+
+//filter by category
+app.get("/all-books", async(req, res) => {
+    let query = {};
+    if(req.query?.category)
+    {
+        query = {category: req.query.category}
+    }
+    const books = bookCollection.find(query);
+    const result = await books.toArray();
+    res.send(result);;
 })
 
 app.get('/', (req, res) => {
