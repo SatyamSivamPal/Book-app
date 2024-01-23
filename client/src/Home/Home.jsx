5b7533037b1b6ed635c2301e999029dfde79a03e
SatyamSivamPal <palsatyam1968@gmail.com>
2024-01-23 19:55:35 +0530

    new

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Banner from '../components/Banner'
 import BestSeller from './BestSeller'
 import FavouriteBook from './FavouriteBook'
+import PromoBanner from './PromoBanner'
+import Others from './Others'
+import Review from './Review'
 
 function Home() {
   return (
@@ -10,6 +13,9 @@ function Home() {
         <Banner />
         <BestSeller /> 
         <FavouriteBook />
+        <PromoBanner />
+        <Others/>
+        <Review />
       </div>
     </div>
   )
---
+++ client/src/Home/Review.jsx
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FaStar } from "react-icons/fa6"
+
+//import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+const Review = () => {
+    return (
+        <div className='my-12 px-4 lg:px-24'>
+            <h2 className='text-5xl font-bold text-center mb-10 leading-snug'>Our Customers</h2>
+            <div>
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50,
+                        },
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <div>
+                            <div className='text-amber-500 flex gap-2'>
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                                <FaStar />
+                            </div>
+                            <div><p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a asperiores quisquam ratione porro voluptates quos debitis atque eligendi totam explicabo, quidem quam, dignissimos deserunt necessitatibus cupiditate quae aliquam similique!</p></div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
+}
+
+export default Review
